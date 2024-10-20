36972bce2fbeff33e34b9b2f9e2a9e594a345463
Alp830 <alpsaruhan96@gmail.com>
2024-10-20 11:46:50 -0700

    Update BlindPeopleApp.py

--- BlindPeopleApp.py
+++ BlindPeopleApp.py
@@ -4,6 +4,7 @@ import google.generativeai as genai
 from google.generativeai.types import HarmCategory, HarmBlockThreshold
 from gtts import gTTS
 from playsound import playsound
+sudo apt-get install python3-gi
 
 
 
