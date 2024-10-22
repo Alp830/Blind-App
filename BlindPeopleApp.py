7310c1139341b299028b4b67e086c0dbcbf81bdc
Alp830 <alpsaruhan96@gmail.com>
2024-10-21 18:58:30 -0700

    Update BlindPeopleApp.py

--- BlindPeopleApp.py
+++ BlindPeopleApp.py
@@ -3,6 +3,21 @@ import os
 import google.generativeai as genai
 from google.generativeai.types import HarmCategory, HarmBlockThreshold
 from gtts import gTTS
+import base64
+
+def autoplay_audio(file_path: str):
+    with open(file_path, "rb") as f:
+        data = f.read()
+        b64 = base64.b64encode(data).decode()
+        md = f"""
+            <audio controls autoplay="true">
+            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+            </audio>
+            """
+        st.markdown(
+            md,
+            unsafe_allow_html=True,
+        )
 
 
 
@@ -86,4 +101,4 @@ if x:
     audio_file_name = "audio.mp3"
     tts = gTTS(responses[0])
     tts.save(audio_file_name)
-    st.audio(audio_file_name, format="audio/mpeg", loop=True)
+    autoplay_audio(audio_file_name)
